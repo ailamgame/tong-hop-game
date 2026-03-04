67a9aceec0f18c1fa94a5cc95cbe9be7d063b72c
namtv95it <111723138+namtv95it@users.noreply.github.com>
2026-03-04 18:08:32 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,6 +7,41 @@ let currentFilter = "all";
    RENDER GAME LIST
 ========================= */
 
+// function renderGames() {
+//     gameGrid.innerHTML = "";
+
+//     games.forEach(game => {
+//         const card = document.createElement("div");
+//         card.classList.add("game-card");
+
+//         const platformText = Array.isArray(game.platform)
+//             ? game.platform.join(" & ")
+//             : game.platform;
+
+//         card.innerHTML = `
+//             <img src="${game.img}" alt="${game.title}">
+//             <div class="game-info">
+//                 <div class="game-text">
+//                     <h3>${game.title}</h3>
+//                     <span>Dành cho: ${platformText}</span>
+//                 </div>
+//                 <button class="play-btn"></button>
+//             </div>
+//         `;
+
+//         // Click nút Play -> chuyển trang
+//         const playBtn = card.querySelector(".play-btn");
+//         playBtn.addEventListener("click", (e) => {
+//             e.stopPropagation();
+//             if (game.link) {
+//                 window.location.href = game.link;
+//             }
+//         });
+
+//         gameGrid.appendChild(card);
+//     });
+// }
+
 function renderGames() {
     gameGrid.innerHTML = "";
 
