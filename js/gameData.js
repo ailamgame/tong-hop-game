eccf3c8bcc10852904af1c0281415f6b37b5bd98
AI Làm Game? <matrix001.dev@gmail.com>
2026-03-04 16:50:08 +0700

    Update gameData.js

--- js/gameData.js
+++ js/gameData.js
@@ -2,30 +2,34 @@
 
 const games = [
   {
-  id: 4,
-  title: "Theo dõi hành trình lớn khôn",
-  platform: ["PC", "Mobile"], // PC | Mobile | ["PC","Mobile"]
-  img: "assets/images/app-baby-days.jpeg",
-  link: "https://ailamgame.github.io/baby-days/", // sửa lại nếu link khác
-},
+    id: 4,
+    title: "Theo dõi hành trình lớn khôn",
+    type: "app", // app | game
+    platform: ["PC", "Mobile"],
+    img: "assets/images/app-baby-days.jpeg",
+    link: "https://ailamgame.github.io/baby-days/",
+  },
   {
-  id: 3,
-  title: "Flappy Kpop",
-  platform: ["PC", "Mobile"], // PC | Mobile | ["PC","Mobile"]
-  img: "assets/images/game-flappy-kpop.jpeg",
-  link: "https://ailamgame.github.io/flappy-kpop/", // sửa lại nếu link khác
-},
+    id: 3,
+    title: "Flappy Kpop",
+    type: "game",
+    platform: ["PC", "Mobile"],
+    img: "assets/images/game-flappy-kpop.jpeg",
+    link: "https://ailamgame.github.io/flappy-kpop/",
+  },
   {
-  id: 2,
-  title: "Flappy Bird",
-  platform: ["PC", "Mobile"], // PC | Mobile | ["PC","Mobile"]
-  img: "assets/images/game-flappy-bird.jpeg",
-  link: "https://ailamgame.github.io/flappy-bird/", // sửa lại nếu link khác
-},
+    id: 2,
+    title: "Flappy Bird",
+    type: "game",
+    platform: ["PC", "Mobile"],
+    img: "assets/images/game-flappy-bird.jpeg",
+    link: "https://ailamgame.github.io/flappy-bird/",
+  },
   {
     id: 1,
     title: "Tank 1990",
-    platform: ["PC"], // PC | Mobile | PC & Mobile
+    type: "game",
+    platform: ["PC"],
     img: "assets/images/game-tank-1990.jpeg",
     link: "https://ailamgame.github.io/tank-1990/",
   },
